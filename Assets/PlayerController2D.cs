a59bb2176415feff33a9f16a5f0ea2ab75a1b433
Enti/Carlos Sierra <carlos.sierra@enti.cat>
2020-01-21 17:06:53 +0100

    Basic Movement Implemented

---
+++ Assets/PlayerController2D.cs
@@ -0,0 +1,243 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerController2D : MonoBehaviour{
+
+    public enum Controller { NONE, PLAYER1, PLAYER2, PLAYER3, PLAYER4 };
+    public Controller controller = Controller.NONE;
+
+    private enum ImpactDirection { NONE, UP, DOWN, RIGHT, LEFT };
+    private ImpactDirection impactDirection = ImpactDirection.NONE;
+
+    public float runSpeed = 2f; 
+    public float jumpStrengh = 6.5f; 
+    public float MAX_Inuminity = 2f;
+    public float MAX_DamageStun = 1f;
+
+
+    //GROUND
+    public bool Grounded; 
+
+    //ATTACK
+    bool Attacking; 
+
+    //DAMAGE
+    bool Damaged;   
+    bool Stunned;   
+    bool Inmune; 
+    
+    float charge = 0f;
+
+    bool IndividualWait = false;
+
+    KeyCode leftButton = KeyCode.None;
+    KeyCode rightButton = KeyCode.None;
+    KeyCode jumpButton = KeyCode.None;
+    KeyCode attackButton = KeyCode.None;
+    KeyCode chargeButton = KeyCode.None;
+
+    private int IdleID;
+    private int RunID;
+    private int JumpID;
+    private int HitID;
+    private int HurtID;
+
+    Animator animator;
+    Rigidbody2D body2D;
+    SpriteRenderer spriteRenderer; //Debug.Log("" + Time.time);
+
+    void Start(){
+        animator = GetComponent<Animator>();
+        body2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        //switch (controller) {
+        //    case Controller.PLAYER1:    //ERROR
+        //        {
+        //            leftButton = KeyCode.A;
+        //        }
+
+        //    default: break;
+        //}
+        if (controller == Controller.PLAYER1)
+        {
+            leftButton = KeyCode.A;
+            rightButton = KeyCode.D;
+            jumpButton = KeyCode.Space;
+            attackButton = KeyCode.Q;
+            chargeButton = KeyCode.Mouse1;
+            IdleID = Animator.StringToHash("Player_Idle");
+            RunID = Animator.StringToHash("Player_Run");
+            //JumpID = Animator.StringToHash("");
+            HitID = Animator.StringToHash("Player_Hit");
+            HurtID = Animator.StringToHash("Player_Hurt");
+            // charge1Anim = animator.Play("Player_Charge1");
+            // charge2Anim = animator.Play("Player_Charge2");
+        }
+        else if (controller == Controller.PLAYER2)
+        {
+            leftButton = KeyCode.LeftArrow;
+            rightButton = KeyCode.RightArrow;
+            jumpButton = KeyCode.UpArrow;
+            attackButton = KeyCode.None;
+            chargeButton = KeyCode.None;
+            IdleID = Animator.StringToHash("Player2_Idle");
+            RunID = Animator.StringToHash("Player2_Run");
+        }
+        else if (controller == Controller.PLAYER3)
+        {
+            leftButton = KeyCode.None;
+            rightButton = KeyCode.None;
+            jumpButton = KeyCode.None;
+            attackButton = KeyCode.None;
+            chargeButton = KeyCode.None;
+        }
+        else if (controller == Controller.PLAYER4)
+        {
+            leftButton = KeyCode.None;
+            rightButton = KeyCode.None;
+            jumpButton = KeyCode.None;
+            attackButton = KeyCode.None;
+            chargeButton = KeyCode.None;
+        }
+    }
+
+    private void FixedUpdate ()
+    {
+    //GetKey: repite cada segundo q se presiona | GetKeyDown: solo one vez al presionar | GetKeyUp: solo one vez al soltar
+    //https://docs.unity3d.com/ScriptReference/KeyCode.html
+        
+        //MOVEMENT
+        if(Input.GetKey(rightButton) && Stunned == false && Attacking == false){
+            body2D.velocity = new Vector2(runSpeed, body2D.velocity.y); //(new x, velocidad actual y)
+            animator.Play(RunID);
+            spriteRenderer.flipX = true;
+        }
+        else if(Input.GetKey(leftButton) && Stunned == false && Attacking == false){
+            body2D.velocity = new Vector2(-runSpeed, body2D.velocity.y);
+            animator.Play(RunID);
+            spriteRenderer.flipX = false;
+        }
+
+        //IDLE
+        else if(Stunned == false && Attacking == false){
+            body2D.velocity = new Vector2(0, body2D.velocity.y); //Si no se pulsa nada = Idle
+            animator.Play(IdleID);
+        }
+
+        //JUMP
+        if(Input.GetKey(jumpButton) && Grounded == true && Stunned == false && Attacking == false){
+            body2D.velocity = new Vector2(body2D.velocity.x, jumpStrengh); //(velocidad actual x, new y)
+        }
+
+        //ATTACK NORMAL
+        if(Input.GetKeyDown(attackButton) && Stunned == false){
+            Attacking = true;
+            animator.Play(HitID);
+            StartCoroutine(ExecuteAfterTime(0.45f, () => {
+                Attacking = false;
+            }));
+        }
+
+        //ATTACK CHARGED
+        if (Input.GetKey(chargeButton) && Stunned == false){
+            Attacking = true;
+            animator.Play("Player_HitCharge1");
+            if (charge < 10f){
+                charge += Time.deltaTime*10f;
+            }
+        }
+        else if (charge > 0){{
+            animator.Play("Player_HitCharge2");
+            StartCoroutine(ExecuteAfterTime(1, () => { Attacking = false; }));
+            charge = 0;
+            }
+        }
+
+        //CHARGED MOVEMENT
+        if(Input.GetKey(rightButton) && Attacking == true){
+            body2D.velocity = new Vector2(runSpeed * 50 / 100, body2D.velocity.y); //(50% de max speed, velocidad actual y)
+            spriteRenderer.flipX = true;
+        }
+        else if(Input.GetKey(leftButton) && Attacking == true){
+            body2D.velocity = new Vector2(-runSpeed * 50 / 100, body2D.velocity.y);
+            spriteRenderer.flipX = false;
+        }
+
+        //DAMAGED
+        if(Damaged == true && Inmune == false && IndividualWait == false){
+            Stunned = true;
+            animator.Play(HurtID);
+            StartCoroutine(ExecuteAfterTime(MAX_DamageStun, () => {
+                Stunned = false;
+                Inmune = true;
+
+                StartCoroutine(ExecuteAfterTime(MAX_Inuminity, () => { Inmune = false; } ));
+
+            } ));
+        }
+
+        IEnumerator ExecuteAfterTime(float seconds, Action task) { //WAIT TIME
+            if (IndividualWait == false || Inmune == true){
+                IndividualWait = true;
+
+                yield return new WaitForSeconds(seconds);
+                task();
+
+                IndividualWait = false;
+            }
+        }   
+    }
+
+    private void OnCollisionStay2D(Collision2D collision){
+
+        Vector3 hit = collision.contacts[0].normal;
+        float angle = Vector3.Angle(hit, Vector3.up);
+
+
+        if (collision.gameObject.tag == "Floor") {
+            
+            if (Mathf.Approximately(angle, 0))
+            {
+                //Down
+                Grounded = true;
+                Debug.Log("Down");
+            }
+            //if (Mathf.Approximately(angle, 180))
+            //{
+            //    //Up
+            //    Debug.Log("Up");
+            //}
+
+            //if (Mathf.Approximately(angle, 90))
+            //{
+            //    // Sides
+            //    Vector3 cross = Vector3.Cross(Vector3.up, hit);
+            //    if (cross.y > 0)
+            //    { // left side of the player
+            //        Debug.Log("Left");
+            //    }
+            //    else
+            //    { // right side of the player
+            //        Debug.Log("Right");
+            //    }
+            //}
+        }
+        
+        if (collision.gameObject.tag == "Damage" && Stunned == false && Inmune == false)   {
+            Damaged = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision){
+        if (collision.gameObject.tag == "Floor") {
+            Grounded = false;
+        }
+
+        if (collision.gameObject.tag == "Damage")   {
+            Damaged = false;
+        }
+    }
+}
