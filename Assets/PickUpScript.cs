4d8b76e644b30f6a6efbaf689a733e65c7c69ead
isthisnametakenyet <carlos.sierra@enti.cat>
2020-02-04 14:03:23 +0100

    weapon placeholders

--- Assets/PickUpScript.cs
+++ Assets/PickUpScript.cs
@@ -5,4 +5,35 @@ using UnityEngine;
 public class PickUpScript : MonoBehaviour
 {
     public PickTypes picktype;
+    
+    public Sprite swordSprite, axeSprite, lanceSprite, bowSprite, crossbowSprite, boomerangSprite;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        switch (picktype)
+        {
+            case PickTypes.Sword:
+                spriteRenderer.sprite = swordSprite;
+                break;
+            case PickTypes.Axe:
+                spriteRenderer.sprite = axeSprite;
+                break;
+            case PickTypes.Lance:
+                spriteRenderer.sprite = lanceSprite;
+                break;
+            case PickTypes.Bow:
+                spriteRenderer.sprite = bowSprite;
+                break;
+            case PickTypes.CrossBow:
+                spriteRenderer.sprite = crossbowSprite;
+                break;
+            case PickTypes.Boomerang:
+                spriteRenderer.sprite = boomerangSprite;
+                break;
+        }
+    }
 }
