bb6940b99b0466b80e98b88e78b811c9c59b1007
Enti/Carlos Sierra <carlos.sierra@enti.cat>
2020-02-15 20:06:20 +0100

    Copy pasted from axe to all other weapons

--- Assets/Scripts/Boomerang.cs
+++ Assets/Scripts/Boomerang.cs
@@ -11,6 +11,12 @@ public class Boomerang : MonoBehaviour
     public bool Attack = false;
     public bool Charging = false;
     public bool Thrown = false;
+    public bool Landed = false;
+    private bool inUse = false;
+
+    //VARIABLES
+    public float AttackTime = 2f;
+    private float actualTime = 0f;
 
     BoxCollider2D collider2D;
     Rigidbody2D body2D;
@@ -20,35 +26,44 @@ public class Boomerang : MonoBehaviour
         collider2D = GetComponent<BoxCollider2D>();
         body2D = GetComponent<Rigidbody2D>();
         Idle = true;
-
-
     }
 
     void FixedUpdate()
     {
-        if (Idle == true && collider2D.enabled == true)
+        if (Idle == true && collider2D.enabled == true && inUse == false && Thrown == false)
         {
+            Debug.Log("Wp: Idle");
+            actualTime = 0f;
             collider2D.enabled = false;
             transform.gameObject.tag = "Weapon";
             //START ANIMATION IDLE
         }
-        else if (Attack == true)
+        else if (Attack == true && inUse == false && Thrown == false)
         {
+            Debug.Log("Wp: Attack");
             collider2D.enabled = true;
             transform.gameObject.tag = "Attacking";
             //START ANIMATION ATTACKING
+            actualTime = 0f;
+            inUse = true;
         }
-        else if (Charging == true)
+        else if (Charging == true && inUse == false && Thrown == false)
         {
-            //FIRST IDLE THEN THIS, ALLWAYS
+            Debug.Log("Wp: Charging");
+            //BEFORE THIS, IDLE, ALLWAYS
             collider2D.enabled = false;
             //START ANIMATION CHARGING
+            inUse = true;
         }
         else if (Thrown == true)
         {
+            Debug.Log("Wp: Thrown");
             collider2D.enabled = true;
             transform.gameObject.tag = "Throwing";
             //START ANIMATION THROW
         }
+
+        if (actualTime <= AttackTime && inUse == true && Charging == false) { actualTime += Time.deltaTime; } //TIEMPO Q DURA LA ANIMACION D ATAQUE
+        else { inUse = false; Idle = true; Attack = false; }
     }
 }
