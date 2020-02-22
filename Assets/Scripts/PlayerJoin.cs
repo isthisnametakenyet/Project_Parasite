8ee2ed360ff163023d2a916b08f8087f157f3a97
Enti/Carlos Sierra <carlos.sierra@enti.cat>
2020-02-22 16:56:30 +0100

    Created Funcion PlayerConnect && PlayerDisconnect

--- Assets/Scripts/PlayerJoin.cs
+++ Assets/Scripts/PlayerJoin.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerJoin : MonoBehaviour
 {
@@ -8,23 +9,36 @@ public class PlayerJoin : MonoBehaviour
     public GameObject Player2Icon;
     public GameObject Player3Icon;
     public GameObject Player4Icon;
+    public GameObject Player1Text;
+    public GameObject Player2Text;
+    public GameObject Player3Text;
+    public GameObject Player4Text;
+    public GameObject PressToJoin;
+
+    string playerNumString;
 
     void Start()
     {
-        
+        Text Player1TXT = Player1Text.GetComponent<Text>();
+        Text Player2TXT = Player2Text.GetComponent<Text>();
+        Text Player3TXT = Player3Text.GetComponent<Text>();
+        Text Player4TXT = Player4Text.GetComponent<Text>();
+        Text PressToJoinTXT = PressToJoin.GetComponent<Text>();
     }
 
     void Update()
     {
-        
-    }
-
-    void Player1Connect()
-    {
 
     }
-    void Player1Disconnect()
-    {
 
+    void PlayerConnect(Text playerTxt, int numPlayer)
+    {
+        playerNumString = numPlayer.ToString();
+        playerTxt.text = "Player " + playerNumString + " Joined";
+    }
+    void PlayerDisconnect(Text playerTxt, int numPlayer)
+    {
+        playerNumString = numPlayer.ToString();
+        playerTxt.text = "Player " + playerNumString + " Disconnected";
     }
 }
