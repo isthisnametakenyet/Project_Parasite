077505f7f4e754c8ca312b660ceb640a81e2446f
Enti/Carlos Sierra <carlos.sierra@enti.cat>
2020-02-22 16:45:40 +0100

    Created Scene PlayerConnect

---
+++ Assets/Scripts/PlayerJoin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJoin : MonoBehaviour
+{
+    public GameObject Player1Icon;
+    public GameObject Player2Icon;
+    public GameObject Player3Icon;
+    public GameObject Player4Icon;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Player1Connect()
+    {
+
+    }
+    void Player1Disconnect()
+    {
+
+    }
+}
