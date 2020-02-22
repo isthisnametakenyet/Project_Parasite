dc84dfd523d4ec1ca8cc8bc25e47aa47296dfe67
Enti/Carlos Sierra <carlos.sierra@enti.cat>
2020-02-22 17:59:38 +0100

    misc presstojoin anim

--- Assets/Scripts/PlayerJoin.cs
+++ Assets/Scripts/PlayerJoin.cs
@@ -15,17 +15,49 @@ public class PlayerJoin : MonoBehaviour
 
     Color selectRed;
     Color disconectRed;
+    Color selectPurple;
+    Color disconectPurple;
+    Color selectYellow;
+    Color disconectYellow;
+    Color selectGreen;
+    Color disconectGreen;
 
     string playerNumString;
 
+    float animDelay;
+
     void Start()
     {
-        //selectRed = new Color(200, 200, 200, 255);
+        //Hexadecimal Colors:
+        //Red ON: FF0000
+        //Red Off 8C0000
+        //Purple ON: 7700E7
+        //Purple Off: 4E0098
+        //Yellow ON: FFFF00
+        //Yellow Off: 828200
+        //Green ON: 00FF00
+        //Green Off: 006A00
+
         ColorUtility.TryParseHtmlString("#FF0000", out selectRed);
-        ColorUtility.TryParseHtmlString("#8C0000", out disconectRed); 
+        ColorUtility.TryParseHtmlString("#8C0000", out disconectRed);
+        ColorUtility.TryParseHtmlString("#7700E7", out selectPurple);
+        ColorUtility.TryParseHtmlString("#4E0098", out disconectPurple);
+        ColorUtility.TryParseHtmlString("#FFFF00", out selectYellow);
+        ColorUtility.TryParseHtmlString("#828200", out disconectYellow);
+        ColorUtility.TryParseHtmlString("#00FF00", out selectGreen);
+        ColorUtility.TryParseHtmlString("#006A00", out disconectGreen);
     }
 
     void Update()
+    {
+        if (animDelay > 0)
+        {
+            animDelay--;
+        }
+        else { animDelay = 1500; }
+    }
+
+    void FixedUpdate()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
@@ -35,6 +67,7 @@ public class PlayerJoin : MonoBehaviour
         {
             PlayerConnect(Player1Icon, selectRed, Player1Text, 1);
         }
+        PressAnim();
     }
 
     void PlayerConnect(GameObject icon, Color colorChange, GameObject playerTxt, int numPlayer)
@@ -57,6 +90,8 @@ public class PlayerJoin : MonoBehaviour
     }
     void PressAnim()
     {
+        Text actualTxt = playerTxt.GetComponent<Text>();
+        //actualTxt.color = ;
 
     }
     void AllConnected(GameObject PressToJoin)
