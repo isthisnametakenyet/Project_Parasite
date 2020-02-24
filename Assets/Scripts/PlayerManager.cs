29e0cce05bed9a3c767a3d7654928891782623d6
isthisnametakenyet <carlos.sierra@enti.cat>
2020-02-24 12:42:54 +0100

    Singleton, Joystick Asigner in progress

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,40 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
-public class PlayerManager : MonoBehaviour
+public class PlayerManager : Singleton <PlayerManager>
 {
-    public static PlayerManager Instance { get; private set; }
-    
-    public int numPlayers;
+    public int numPlayers = 4;
     public int[] scorePlayers;
     public bool Player1ON;
     public bool Player2ON;
     public bool Player3ON;
     public bool Player4ON;
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(this);
-        } else {
-            Debug.Log("WARNING: multiple " + this + " in scene");
-        }
-    }
-
-    public void AddPlayer() //MENU 
-    {
-        numPlayers++;
-
-    }
-
-    public void DeletePlayer() //MENU
-    {
-        numPlayers--;
-
-    }
+    public int asignPlayerID;
 
     public void StartGame() //GAME
     {
