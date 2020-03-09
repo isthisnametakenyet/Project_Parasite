f706e4b879b1523d1b8b38fd152d3cb629829da9
OriolB-Uri <56065427+OriolB-Uri@users.noreply.github.com>
2020-03-09 14:25:09 +0100

    Enlaces entre escenas

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine;
 
-public class MainMenuController : MonoBehaviour
+public class SceneController : MonoBehaviour
 {
     public void ChangeScene(string name)
     {
         SceneManager.LoadScene(name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
