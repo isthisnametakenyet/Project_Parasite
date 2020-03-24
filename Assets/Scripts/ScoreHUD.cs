8fbdddec74f9cf2a04caa1529d06a1cffc68008e
Enti/Carlos Sierra <carlos.sierra@enti.cat>
2020-03-24 23:39:53 +0100

    Skin1 reanimate (cus someone deleted the aniamtions i made)

--- Assets/Scripts/ScoreHUD.cs
+++ Assets/Scripts/ScoreHUD.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScoreHUD : MonoBehaviour
 {
@@ -423,5 +424,6 @@ public class ScoreHUD : MonoBehaviour
     {
         yield return new WaitForSeconds(delayEnd);
         //CARGAR MAIN MENU
+        SceneManager.LoadScene("MainMenu");
     }
 }
