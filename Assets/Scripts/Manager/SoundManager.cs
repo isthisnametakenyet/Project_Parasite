3afbf7a853fa9fdbfd4bb34fb61faf6d7848b564
Enti/Carlos Sierra <carlos.sierra@enti.cat>
2020-04-28 12:57:44 +0200

    regulateAll in Init

--- Assets/Scripts/Manager/SoundManager.cs
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -32,6 +32,8 @@ public class SoundManager : MonoBehaviour
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
         }
+
+        RegulateAll(MusicVolume, SFXVolume);
     }
 
     public void Play(string name)
