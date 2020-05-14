9439d619a2a80d240b609bef11bee3b8cec44836
AlexAlvarezAlmendros <alexalmendrosal@gmail.com>
2020-05-14 10:07:28 +0200

    Button Sounds

---
+++ Assets/Scripts/buttonFx.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonFx : MonoBehaviour
+{
+    public void Sound() {
+
+        SoundManager.instance.Play("ButtonSFX");
+
+    }
+
+
+}
