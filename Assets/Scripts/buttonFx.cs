d79046ecfd7091dec56653d41e64e50aac000b47
Enti/Carlos Sierra <carlos.sierra@enti.cat>
2020-06-10 13:55:40 +0200

    main menu select brush1

--- Assets/Scripts/buttonFx.cs
+++ Assets/Scripts/buttonFx.cs
@@ -1,14 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class buttonFx : MonoBehaviour
 {
+
     public void Sound() {
 
         SoundManager.instance.Play("ButtonSFX");
 
     }
 
+    TextMeshProUGUI textChild;
 
+    public void SelectChild(TextMeshProUGUI textChild2)
+    {
+        textChild = textChild2;
+    }
+
+    public void ChangeMTPColor(string SelectedColor/*, Text textChild*/)
+    {
+        Color selectColor;
+        ColorUtility.TryParseHtmlString(SelectedColor, out selectColor);
+        textChild.color = selectColor;
+        textChild = null;
+    }
 }
